feb24735986afe5bcdb6e16c52f7e7a2cf16775d
Amir Off <ameer157@gmail.com>
2018-06-15 15:06:18 +0300

    Improve documentation

--- src/src.ino
+++ src/src.ino
@@ -1,15 +1,28 @@
 /*
     MIDI Controller on Arduino (Pro) Micro 5V/16MHz
 
-    MIDI Out mappings:
+       This is a generic MIDI controller for DJs and music producers built on ATmega32u4 microcontroller.
+       The controller has been tested on Native Instrument's Traktor Pro 2 and Ableton Live 10 and should work on other versions alike.
+
+       It doesn't require any external software such as "The Hairless MIDI" since this is MIDI USB Class Compliant device.
+
+      MIDI mapping:
+
+       MIDI In can be assigned from the software to any knob, slide or button you map it to,
+       but MIDI Out has to be configured in the readMIDI() method in the code.
+
+      Traktor Pro 2:
        MIDI Channel = 4
        MIDI CC 80 = Filter On (Deck A)
        MIDI CC 81 = Monitor Cue On (Deck A)
+       MIDI CC 84 = Deck Focus Selector (Deck A)
        MIDI CC 82 = Filter On (Deck B)
+       MIDI CC 83 = Monitor Cue On (Deck B)
+       MIDI CC 85 = Deck Focus Selector (Deck B)
 
-    Traktor Pro 2 mappings file: https://goo.gl/JWAb5h55pk
+     Mappings file: https://goo.gl/JWAb5h55pk
 
-    Electronic components used:
+     Electronic components used:
        1. Arduino (Pro) Micro 5V/16MHz (ATmega32u4) (1x)
        2. Rotary encoder (1x)
        3. 10k Rotary potentiometer (8x)
@@ -18,15 +31,16 @@
        6. Standard (3501) Blue 3mm diffused LEDs (4x blue, 2x green)
        7. 16-Channel Analog/Digital CD74HC4067 Multiplexer (1x)
 
-    Fritzing schematics file: https://goo.gl/JW5b5455pk
+      Fritzing schematics file: https://goo.gl/JW5b5455pk
 
-    Created 09/05/2018
-    By Amir Off
-    Modified 15/06/2018
-    By Amir Off
+     USB device name change:
 
-    This is a MIDI USB Class Compliant device
+       If you want to change your MIDI controller device name go ahead and read this: http://liveelectronics.musinou.net/MIDIdeviceName.php
 
+     Created 09/05/2018
+     By Amir Off
+     Modified 15/06/2018
+     By Amir Off
 */
 
 ///////////////////////
